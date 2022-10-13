8185020477cf675bccff15e566eb22bcf87fd63b
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-13 14:30:22 +0530

    Notice init

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Services.Configure<FuelDatabaseSettings>(
     builder.Configuration.GetSection("FuelDatabaseSettings"));
 
 builder.Services.AddSingleton<UserService>();
+builder.Services.AddSingleton<NoticeService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
