588283732f3830a1c0bcefa6a892f5858f6040ee
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-15 19:51:41 +0530

    Create FuelStation.cs

---
+++ Models/FuelStation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace FuelAppAPI.Models
+{
+    public class FuelStation
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string? License { get; set; }
+        public string? OwnerUsername { get; set; }
+        public string? StationName { get; set; }
+        public string? StationAddress { get; set; }
+        public string? StationPhoneNumber { get; set; }
+        public string? StationEmail { get; set; }
+        public string? StationWebsite { get; set; }
+
+        public string? OpenStatus { get; set; }
+        public int? QueueLength { get; set; }
+        public string? PetrolStauts { get; set; }
+        public string? DieselStauts { get; set; }
+    }
+}
+
