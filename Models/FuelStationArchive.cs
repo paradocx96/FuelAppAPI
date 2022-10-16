8adc4e66007194bb402fc5890d507b544d6ea303
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-16 13:54:13 +0530

    Create FuelStationArchive.cs

---
+++ Models/FuelStationArchive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+/*
+ * IT19014128
+ * A.M.W.W.R.L. Wataketiya
+ * 
+ * Model class for Fuel Station Archive
+ */
+namespace FuelAppAPI.Models
+{
+    public class FuelStationArchive
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string? License { get; set; }
+        public string? OwnerUsername { get; set; }
+        public string? StationName { get; set; }
+        public string? StationAddress { get; set; }
+        public string? StationPhoneNumber { get; set; }
+        public string? StationEmail { get; set; }
+        public string? StationWebsite { get; set; }
+    }
+}
+
