b29ae24eb1138947384c0051fed26327586522fa
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-16 09:57:15 +0530

    Database Config Update

--- Models/Database/FuelDatabaseSettings.cs
+++ Models/Database/FuelDatabaseSettings.cs
@@ -1,10 +1,16 @@
 namespace FuelAppAPI.Models.Database;
 
-public class FuelDatabaseSettings
+public class FuelDatabaseSettings : IFuelDatabaseSettings
 {
     public string DatabaseName { get; set; }
     
     public string ConnectionString { get; set; }
-
+    
+    public string UserCollectionName { get; set; }
+    
+    public string NoticeCollectionName { get; set; }
+    
+    public string FeedbackCollectionName { get; set; }
+    
     public string FuelStationsCollectionName { get; set; } // Name for the fuel station collection
 }
