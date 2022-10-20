5771fb1d95f317c79ef137a549f5b20c5b29b38a
Hirush Gimhan <it19240848@my.sliit.lk>
2022-10-21 01:00:32 +0530

    Added new attributes for favourite model

--- Models/Favourite.cs
+++ Models/Favourite.cs
@@ -1,6 +1,17 @@
-﻿namespace FuelAppAPI.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FuelAppAPI.Models
 {
     public class Favourite
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string StationId { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string StationName { get; set; } = null!;
+        public string StationAddress { get; set; } = null!;
+        public string CreateAt { get; set; } = null!;
     }
 }
