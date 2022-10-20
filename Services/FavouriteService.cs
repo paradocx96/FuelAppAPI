a26b51b9b83b15c06a3125ee9a3d1f82b4a47b76
Hirush Gimhan <it19240848@my.sliit.lk>
2022-10-21 01:18:41 +0530

    Configured database connection for Favourite

--- Services/FavouriteService.cs
+++ Services/FavouriteService.cs
@@ -1,6 +1,25 @@
-﻿namespace FuelAppAPI.Services
+﻿using FuelAppAPI.Models;
+using FuelAppAPI.Models.Database;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace FuelAppAPI.Services
 {
     public class FavouriteService
     {
+        //Create mongodb collection references
+        private readonly IMongoCollection<Favourite> _favouriteCollection;
+        private readonly IMongoCollection<BsonDocument> _collection;
+
+        public FavouriteService(IOptions<FuelDatabaseSettings> fuelDatabaseSettings) {
+
+            var monogClient = new MongoClient(fuelDatabaseSettings.Value.ConnectionString);
+            var mongoDatabase = monogClient.GetDatabase(fuelDatabaseSettings.Value.DatabaseName);
+
+            _favouriteCollection = mongoDatabase.GetCollection<Favourite>(fuelDatabaseSettings.Value.FavouriteCollectionName);
+            _collection = mongoDatabase.GetCollection<BsonDocument>(fuelDatabaseSettings.Value.FavouriteCollectionName);
+
+        }
     }
 }
