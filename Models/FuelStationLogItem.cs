ff8b4a8e2d95d0ac663730758eb395f09d4bad21
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 01:11:12 +0530

    Update FuelStationLogItem.cs

--- Models/FuelStationLogItem.cs
+++ Models/FuelStationLogItem.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 /*
  * IT19014128
@@ -11,6 +13,8 @@ namespace FuelAppAPI.Models
 {
     public class FuelStationLogItem
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         public string? StationId { get; set; }
         public string? FuelType { get; set; }
