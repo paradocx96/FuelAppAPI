03fbd97d49673b2d2598c0547a172f8a242b2741
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-23 19:22:26 +0530

    Create QueueLogItemDto.cs

---
+++ DTO/QueueLogItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FuelAppAPI.DTO
+{
+    public class QueueLogItemDto
+    {
+        public string? Id { get; set; }
+
+        public string? CustomerUsername { get; set; }
+        public string? StationId { get; set; }
+        public string? StationLicense { get; set; }
+        public string? StationName { get; set; }
+        public string? Queue { get; set; }
+        public string? Action { get; set; }  //join / leave
+        public string? RefuelStatus { get; set; } //refuled / not-refueled / not-applicable
+    }
+}
+
