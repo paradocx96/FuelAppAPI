1c0835e3ba4f93121058af2f4d30715fff482040
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 01:21:27 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSingleton<FeedbackService>();
 builder.Services.AddSingleton<FuelStationService>();
 builder.Services.AddSingleton<FuelStationArchiveService>();
 builder.Services.AddSingleton<QueueLogService>();
+builder.Services.AddSingleton<FuelStationService>();
 
 
 builder.Services.AddControllers();
