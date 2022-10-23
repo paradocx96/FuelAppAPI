6c503f77387387deafa669d82c8cd9b942a70268
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 01:05:19 +0530

    Create FuelStationLogItem.cs

---
+++ Models/FuelStationLogItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * IT19014128
+ * A.M.W.W.R.L. Wataketiya
+ * 
+ * Model for Fuel Statio Log Item
+ */
+
+namespace FuelAppAPI.Models
+{
+    public class FuelStationLogItem
+    {
+        public string? Id { get; set; }
+        public string? StationId { get; set; }
+        public string? FuelType { get; set; }
+        public string? FuelStatus { get; set; }
+        public DateTime? DateTime { get; set; }
+    }
+}
+
