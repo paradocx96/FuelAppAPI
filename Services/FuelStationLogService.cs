888f1aa46a5a22086846fe51cbb25c0c3a5cecb2
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 01:16:19 +0530

    Create FuelStationLogService.cs

---
+++ Services/FuelStationLogService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FuelAppAPI.Models;
+using FuelAppAPI.Models.Database;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+/*
+ * IT19014128
+ * A.M.W.W.R.L. Wataketiya
+ * 
+ * Service class for Fuel station logs
+ */
+
+namespace FuelAppAPI.Services
+{
+    public class FuelStationLogService
+    {
+        private readonly IMongoCollection<FuelStationLogItem> _fuelStationLogItemCollection;
+        public FuelStationLogService(IOptions<FuelDatabaseSettings> fuelDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(fuelDatabaseSettings.Value.ConnectionString); //initialize mongo client
+            var mongoDatabase = mongoClient.GetDatabase(fuelDatabaseSettings.Value.DatabaseName); //initialize mongo database
+            _fuelStationLogItemCollection = mongoDatabase.GetCollection<FuelStationLogItem>(fuelDatabaseSettings.Value.QueueLogsCollectionName); //assign the collection
+        }
+    }
+}
+
