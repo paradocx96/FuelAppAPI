5f217d7580f6765acd29faad22194c6d446f1253
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-23 19:42:53 +0530

    Create QueueLogService.cs

---
+++ Services/QueueLogService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FuelAppAPI.Models;
+using MongoDB.Driver;
+
+namespace FuelAppAPI.Services
+{
+    public class QueueLogService
+    {
+        private readonly IMongoCollection<QueueLogItem> _queueLogItemCollection;
+        public QueueLogService()
+        {
+        }
+    }
+}
+
