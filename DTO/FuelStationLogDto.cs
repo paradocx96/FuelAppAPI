df50e6fa25ede1cdfe747bb1ab99c432449419ea
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 01:12:32 +0530

    Create FuelStationLogDto.cs

---
+++ DTO/FuelStationLogDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * IT19014128
+ * A.M.W.W.R.L. Wataketiya
+ * 
+ * DTO for Fuel Station Log Item
+ */
+
+namespace FuelAppAPI.DTO
+{
+    public class FuelStationLogDto
+    {
+        public string? Id { get; set; }
+        public string? StationId { get; set; }
+        public string? FuelType { get; set; }
+        public string? FuelStatus { get; set; }
+        public DateTime? DateTime { get; set; }
+        public int? Year { get; set; }
+        public int? Month { get; set; }
+        public int? DayNumber { get; set; }
+        public int? Hour { get; set; }
+        public int? Minute { get; set; }
+        public int? Second { get; set; }
+    }
+}
+
